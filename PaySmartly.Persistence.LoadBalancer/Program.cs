5351c72a06b204cc9f33bb20be55d0a18b095088
stefan <stefan.bozov@outlook.com>
2023-12-03 17:13:42 +0200

    add OpenTelemetry

--- PaySmartly.Persistence.LoadBalancer/Program.cs
+++ PaySmartly.Persistence.LoadBalancer/Program.cs
@@ -1,9 +1,47 @@
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+// TODO: set service name somewhere!!!
+string ServiceName = "Persistance.LoadBalancer Service";
+
 var builder = WebApplication.CreateSlimBuilder(args);
+AddOpenTelemetryLogging(builder);
 
 builder.Services
    .AddReverseProxy()
    .LoadFromConfig(builder.Configuration.GetSection("LoadBalancer"));
 
+AddOpenTelemetryService(builder);
+
 var app = builder.Build();
 app.MapReverseProxy();
 app.Run();
+
+void AddOpenTelemetryLogging(WebApplicationBuilder builder)
+{
+   builder.Logging.AddOpenTelemetry(options =>
+   {
+      ResourceBuilder resourceBuilder = ResourceBuilder.CreateDefault().AddService(ServiceName);
+
+      options.SetResourceBuilder(resourceBuilder).AddConsoleExporter();
+   });
+}
+
+void AddOpenTelemetryService(WebApplicationBuilder builder)
+{
+   OpenTelemetryBuilder openTelemetryBuilder = builder.Services.AddOpenTelemetry();
+
+   openTelemetryBuilder = openTelemetryBuilder.ConfigureResource(resource => resource.AddService(ServiceName));
+
+   openTelemetryBuilder = openTelemetryBuilder.WithTracing(tracing =>
+   {
+      tracing.AddAspNetCoreInstrumentation().AddConsoleExporter();
+   });
+   openTelemetryBuilder = openTelemetryBuilder.WithMetrics(metrics =>
+   {
+      metrics.AddAspNetCoreInstrumentation().AddConsoleExporter();
+   });
+}
